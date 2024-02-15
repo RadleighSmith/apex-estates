366999f7edc5f18f04597ec91b08a47619862040
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-15 19:04:01 +0000

    Add initial property model

--- apex_estates/settings.py
+++ apex_estates/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
@@ -90,6 +90,8 @@ WSGI_APPLICATION = 'apex_estates.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
 
 
 # Password validation
--- property/models.py
+++ property/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STRUCTURE_CATEGORY = (
+    (0, "Detached House"),
+    (1, "Semi-Detached House"),
+    (2, "Terraced House"),
+    (3, "Apartment"),
+    (4, "Flat"),
+    (5, "Studio Apartment"),
+    (6, "Penthouse"),
+    (7, "Townhouse"),
+)
+
+class Property(models.Model):
+    property_type = models.IntegerField(choices=STRUCTURE_CATEGORY, default=0)
+    location = models.CharField(max_length=100)
+    price = models.IntegerField()
+    bedrooms = models.PositiveIntegerField(default=0)
+    bathrooms = models.PositiveIntegerField(default=0)
+    garage = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+    description = models.TextField()
+    listed_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
