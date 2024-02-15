f530e11fb7c78e441f50e8fe988125f4f54cca40
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-15 19:47:52 +0000

    Add initial user favourites model

--- favourite/models.py
+++ favourite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from property.models import Property
 
-# Create your models here.
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'property')
