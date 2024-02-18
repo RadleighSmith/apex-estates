55f82fedc597beb1e41c3b299a821b19e9f16268
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-18 17:42:00 +0000

    Add logout feature

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
 def home(request):
