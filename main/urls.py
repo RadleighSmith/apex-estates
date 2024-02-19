ce71453ef8049de64ff3d650dfa11387068dff43
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-19 18:57:33 +0000

    Add about us and contact us pages

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about_us/', views.about, name='about_us'),
+    path('contact_us/', views.contact, name='contact_us'),
 ]
