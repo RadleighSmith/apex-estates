ec65d7832b42c0b3e242d077bde9a9ccbd04307e
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-20 18:35:29 +0000

    FIX typo in settings.py file

--- apex_estates/settings.py
+++ apex_estates/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': d[
+            'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
