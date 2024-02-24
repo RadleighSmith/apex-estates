198580e3c49238d9739eca82fbb11b2726815c7e
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-24 20:07:24 +0000

    FIX: Issue where price was not showing on homepage cards

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.humanize.templatetags.humanize import intcomma
 from property.models import Property
 
 # Create your views here.
 
 def home(request):
     latest_properties = Property.objects.order_by('-listed_on')[:3]
+    for property in latest_properties:
+        property.formatted_price = intcomma(property.price) 
     return render(request, 'index.html', {'latest_properties': latest_properties})
 
 def about(request):
