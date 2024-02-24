9522e55975a2f2d2e5fedfe651a690f948c94b88
Radleigh Smith <RadleighMSmith@gmail.com>
2024-02-24 13:27:24 +0000

    Add latest properties section into home page design

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from property.models import Property
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    latest_properties = Property.objects.order_by('-listed_on')[:3]
+    return render(request, 'index.html', {'latest_properties': latest_properties})
 
 def about(request):
     return render(request, 'about.html')
