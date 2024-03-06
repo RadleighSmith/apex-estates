7a492c7d880f2c7a25194e505fb5cf2b639cf6d4
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-06 20:32:13 +0000

    Add signup messages

--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.humanize.templatetags.humanize import intcomma
 from django.core.paginator import Paginator
 from property.models import Property
+from .forms import RegisterForm
 
 # Create your views here.
 
@@ -14,7 +15,10 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, 'You have successfully signed up!')
             return redirect('/')
+        else:
+            messages.error(request, 'There was an error with your sign-up. Please try again.')
     else:
         form = RegisterForm()
 
@@ -27,9 +31,8 @@ def user_dashboard(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    # Add formatted_price and is_favourite attributes to each property in user_favourites queryset
     for property in page_obj.object_list:
         property.formatted_price = intcomma(property.price)
-        property.is_favourite = True  # Assuming all properties in user_favourites are already favorited by the user
+        property.is_favourite = True
 
     return render(request, 'dashboard/dashboard.html', {'page_obj': page_obj})
