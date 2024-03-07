6fa0cb32666fe6b9e3d6a9bb9c8965949cab5271
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-07 14:52:47 +0000

    Add message form

---
+++ main/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Message
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['first_name', 'last_name', 'email', 'message']
