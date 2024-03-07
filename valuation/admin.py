2eb1d92d8ac3924c08237a836f1f07c930f114fc
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-07 17:53:44 +0000

    Add valuation to admin page

--- valuation/admin.py
+++ valuation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ValuationRequest
 
-# Register your models here.
+@admin.register(ValuationRequest)
+class ValuationRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_number', 'property_address', 'email', 'property_type')
+    search_fields = ('name', 'contact_number', 'property_address', 'email', 'property_type')
