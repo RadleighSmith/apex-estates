a081cdfc2d59db8b9ae1d5a8ca24422693c0fd72
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-07 15:20:40 +0000

    Add tests to main app

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class MessageViewsTestCase(TestCase):
+    def test_about_view(self):
+        url = reverse('about_us')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_view(self):
+        url = reverse('contact_us')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_send_message_view(self):
+        url = reverse('send_message')
+        form_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'john@example.com',
+            'message': 'Test Message',
+        }
+        response = self.client.post(url, form_data)
+        self.assertEqual(response.status_code, 302)
