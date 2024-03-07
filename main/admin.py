2d72825f7717dcd0a89dff2ed3dc0f183c18fbcd
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-07 14:53:26 +0000

    Add message admin structure

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email', 'message')
+
+admin.site.register(Message, MessageAdmin)
