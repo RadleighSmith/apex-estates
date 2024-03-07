2646f4a14839b0838d57c5b9c0a4a28124890804
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-07 17:53:58 +0000

    Amended main message admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Message
 
+@admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'created_at')
     search_fields = ('first_name', 'last_name', 'email', 'message')
-
-admin.site.register(Message, MessageAdmin)
