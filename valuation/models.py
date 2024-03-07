825335327fe37992bb0beb5fd2c1fd41ab8476e8
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-07 17:28:39 +0000

    Add valuation model

--- valuation/models.py
+++ valuation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class ValuationRequest(models.Model):
+    name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    property_address = models.CharField(max_length=255)
+    email = models.EmailField()
+    PROPERTY_TYPES = [
+        ('house', 'House'),
+        ('apartment', 'Apartment'),
+        ('land', 'Land'),
+        ('commercial', 'Commercial Property'),
+        ('other', 'Other')
+    ]
+    property_type = models.CharField(max_length=20, choices=PROPERTY_TYPES)
+
+    def __str__(self):
+        return self.name
