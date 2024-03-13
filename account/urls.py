66dadeca87b43cd2667a2e41d81f6b47141e94c4
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-13 22:29:11 +0000

    Add PEP8 standard layout to urls

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('dashboard/', views.user_dashboard, name='dashboard'),
