a5a8e488e6c0994a330610c3e361c45874846c96
Radleigh Smith <RadleighMSmith@gmail.com>
2024-03-14 10:27:36 +0000

    Add docstring to forms and pep8 structure to urls and admin files

--- property/forms.py
+++ property/forms.py
@@ -1,12 +1,37 @@
 from django import forms
 from .models import Property
 
+
 class PropertyForm(forms.ModelForm):
+    """
+    Form for creating or updating a property.
+
+    This form extends the ModelForm class and is used to create or update
+    Property objects. It includes fields for the title, main image, property
+    type, address, price, number of bedrooms, number of bathrooms, garage
+    availability, parking availability, and description of the property.
+
+    Attributes:
+        description (forms.CharField): A CharField widget with a TextArea
+            attribute to allow multi-line input for the property description.
+    """
+
     description = forms.CharField(widget=forms.Textarea(attrs={'id': 'description'}))
-    
+
     class Meta:
+        """
+        Meta class specifying the model and fields for the form.
+
+        Attributes:
+            model (class): The model class to be used for the form.
+            fields (list): A list of field names to include in the form.
+            widgets (dict): A dictionary specifying custom widgets for form
+                fields.
+        """
+
         model = Property
-        fields = ['title', 'main_image', 'property_type', 'address', 'price', 'bedrooms', 'bathrooms', 'garage', 'parking', 'description']
+        fields = ['title', 'main_image', 'property_type', 'address', 'price',
+                  'bedrooms', 'bathrooms', 'garage', 'parking', 'description']
         widgets = {
             'title': forms.TextInput(attrs={'maxlength': 200}),
             'address': forms.TextInput(attrs={'maxlength': 100}),
--- property/urls.py
+++ property/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('property_listings/', views.PropertyList.as_view(), name='property_listings'),
     path('properties/<slug:slug>/', views.PropertyDetail.as_view(), name='property_detail'),
