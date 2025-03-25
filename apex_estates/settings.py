3a4b62e06048cae7274fa7e67ffc1838d8c522b0
RadleighSmith <RadleighMSmith@gmail.com>
2025-03-25 20:41:26 +0000

    Fix missing config in database config

--- apex_estates/settings.py
+++ apex_estates/settings.py
@@ -100,8 +100,9 @@ WSGI_APPLICATION = 'apex_estates.wsgi.application'
 
 # Database configuration
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
+
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
